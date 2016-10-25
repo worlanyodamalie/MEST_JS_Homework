da7e920af7338715638fd704313980d9057b2ee8
Worlanyo <worlalidamalie@gmail.com>
2016-10-25 08:16:18 +0000

    Update lecture1.js

--- lecture1.js
+++ lecture1.js
@@ -60,12 +60,88 @@ var response = "Ashwin said, Yes Sir!";/*the presence of  extras quote resulted 
  *  5. Build a Animal object by using object constructor function, and add at least two method to prototype,
  *     create two animals from it.
  */
+  function Animal (mammal,type,size,habitat)
+	this.mammal = mammal;
+	this.type = type;
+	this.size = size;
+	this.habitat=habitat;
+
+}
+
+Animal.prototype.view(){
+	return this.mammal
+
+};
+
+Animal.prototype.type(){
+	return this.type + ' stay in' + this.habitat
+
+};
+
+
+animal1 = new Animal('cat','cuteblack','small','house')
+
+animal1.view(
+
 /*
  * 6. Write a small menu selection function by using if-else and switch, input number, output name of your order
  */
+	var item = prompt('menu items','enter item number')
+switch (item)
+		case 1:
+		alert('Waakye is 3 cedis');
+		break;
+		case 2:
+		alert('Akple is 5 cedis')
+		break;
+		default:
+		alert('item you have selected is not in the menu')
+		break;
+
+
+
+
+//ifeslse menu
+
+var item = prompt('menu items', 'enter item number')
+	if (item === 1){
+		
+			alert('Waakye is 3 cedis')
+			}
+		else if(item === 2){
+			alert('Akple is 5 cedis') 
+	        }
+	    else {
+	    	alert('item not in menu')
+	    }
+
+	
 /*
  * 7. var studentScores = [50, 67, 80, 90, 100, 30, 88, 68]; write a program by using studentScores array ,for loop and conditions.
  */
+    
+   var studentScores = [50,67, 80, 90, 100, 30, 88, 68];
+   for (i =0; i<studentScores.length;i++){
+			if (studentScores[i] <= 50){
+			
+				alert('fail')
+				}
+			else if(studentScores[i] >=51 && studentScores[i] <= 80 ){
+				alert('pass') 
+		        }
+		    else if (studentScores[i] >= 81 && studentScores[i]<= 100){
+		    	alert('excellent')
+		    }
+		    else{
+		    	alert('marks not in range')
+		    }
 /*
  * 8. var studentScores = [50, 67, 80, 90, 100, 30, 88, 68]; write a program by using studentScores array , array.forEach and conditions.
  */
+   var sum = 0;
+ 	for each (var score in studentScores){
+ 		sum += score
+ 		return sum
+
+ 	}
+	   
